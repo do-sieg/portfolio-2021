1a0327100cb81b2e37594dd0fd604d3002384589
do-sieg <saleth.admin@gmail.com>
2021-11-26 02:45:15 +0100

    Changed picture

--- components/app/Hero.js
+++ components/app/Hero.js
@@ -1,12 +1,13 @@
+import { DEV_FIRST_NAME, DEV_LAST_NAME } from "../../data/constants";
 import styles from "./Hero.module.css";
 
 export default function Hero() {
     return (
         <div className={styles.container}>
-            <img src="./images/profile/profile_01.jpg" alt="Daniel Orchanian" />
+            <img src="./images/profile/profile_03.jpg" alt="Daniel Orchanian" />
             <div className={styles.typography}>
-                <div className={styles.firstName}>Daniel</div>
-                <div className={styles.lastName}>Orchanian</div>
+                <div className={styles.firstName}>{DEV_FIRST_NAME}</div>
+                <div className={styles.lastName}>{DEV_LAST_NAME}</div>
             </div>
         </div>
     );
--- data/constants.js
+++ data/constants.js
@@ -1,4 +1,6 @@
 export const SITE_TITLE = "Daniel Orchanian";
+export const DEV_FIRST_NAME = "Daniel";
+export const DEV_LAST_NAME = "Orchanian";
 export const DEV_EMAIL = "d.orchanian@gmail.com";
 export const DEV_GITHUB = "https://github.com/do-sieg";
 export const DEV_LINKEDIN = "http://www.linkedin.com/in/daniel-orchanian";
