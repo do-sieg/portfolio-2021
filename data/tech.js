9271dfff9457d7402f7c6134935aa189233b10d2
do-sieg <saleth.admin@gmail.com>
2022-01-30 10:36:51 +0100

    Updated packages

--- data/tech.js
+++ data/tech.js
@@ -1,7 +1,7 @@
 import { DiRuby } from "react-icons/di";
 import { FaCss3Alt, FaGitAlt, FaHtml5, FaJsSquare, FaLess, FaNodeJs, FaPaperPlane, FaPhp, FaReact, FaSass, FaSearchengin, FaWordpress } from "react-icons/fa";
 import { GrMysql } from "react-icons/gr";
-import { SiGodotengine, SiMongodb, SiNextDotJs } from "react-icons/si";
+import { SiGodotengine, SiMongodb, SiNextdotjs } from "react-icons/si";
 
 export default {
     html: { name: "HTML", icon: <FaHtml5 /> },
@@ -11,7 +11,7 @@ export default {
     js: { name: "JavaScript", icon: <FaJsSquare /> },
     reactjs: { name: "ReactJS", icon: <FaReact /> },
     nodejs: { name: "NodeJS", icon: <FaNodeJs /> },
-    nextjs: { name: "NextJS", icon: <SiNextDotJs /> },
+    nextjs: { name: "NextJS", icon: <SiNextdotjs /> },
     mysql: { name: "MySQL", icon: <GrMysql /> },
     php: { name: "PHP", icon: <FaPhp /> },
     ruby: { name: "Ruby", icon: <DiRuby /> },
